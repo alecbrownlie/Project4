ff15087d95e274ef63cf44a2c8918f4ac5993146
Alec Brownlie <alecbrownlie@gmail.com>
2019-04-30 20:39:31 -0700

    first commit

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# run Project 4 Application
+cd src
+# export CLASSPATH=$CLASSPATH:lib/jcommon-1.0.23.jar:lib/jfreechart-1.0.19.jar 
+javac main/Application.java
+java main/Application
+cd ..
+rm src/*/*.class
---
+++ src/main/Application.java
@@ -0,0 +1,55 @@
+package main;
+import java.util.Scanner;
+
+public class Application {
+	private static String PROJECT_BANNER = "----- Project 04: Knapsack -----";
+	private static String INSTRUCTIONS = "Please select task:";
+	private static String WELCOME_MSG = "      Welcome! " + INSTRUCTIONS;
+	private static String KNAPSACK_MODE = "Knapsack Solutions.";
+	private static String COMPARISON_MODE = "Knapsack Efficiency Graphs";
+
+	// private static KnapsackMode knapsackMode = new KnapsackMode();
+	// private static ComparisonMode comparisonMode = new ComparisonMode();
+
+	public static void main(String[] args) {
+		printMenu();
+		processUserInput();
+	}
+
+	private static void printMenu() {
+		System.out.println(PROJECT_BANNER);
+		System.out.println(WELCOME_MSG);
+	}
+
+	private static void printChoices() {
+		System.out.println("1)    " + KNAPSACK_MODE);
+		System.out.println("2)    " + COMPARISON_MODE);
+	}
+
+	private static void processUserInput() {
+		Scanner scanner = new Scanner(System.in);
+		boolean toProcess = true;
+		try {
+			while (toProcess) {
+				printChoices();
+				switch (Integer.parseInt(scanner.nextLine())) {
+					case 1:
+						System.out.println("Selected: " + KNAPSACK_MODE);
+						// knapsackMode.run();
+						toProcess = false;
+						break;
+					case 2: 
+						System.out.println("Selected: " + COMPARISON_MODE);
+						// comparisonMode.run();
+						toProcess = false;
+						break;
+					default:
+						System.out.println("Not a choice. " + INSTRUCTIONS);
+				}	
+			}
+		} catch (java.lang.NumberFormatException e) {
+			System.out.println("Not a number. " + INSTRUCTIONS);
+			processUserInput();
+		}
+	}
+}
