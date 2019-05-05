e100eff2aac72ff4c8fc54f2db51b7405f10a12d
Alec Brownlie <alecbrownlie@gmail.com>
2019-05-05 08:22:28 -0700

    fixing script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # run Project 4 Application
-rm test/.DS_Store
+rm test/.DS_Store 2> /dev/null
 cd src
 export CLASSPATH=$CLASSPATH:lib/jcommon-1.0.23.jar:lib/jfreechart-1.0.19.jar 
 javac main/Application.java
